27c79a085233b60bc2b72c28e787c0e05d74396e
shashwat9205 <shashwat.mishra1010101@gmail.com>
2026-05-26 15:29:31 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 // src/config.js
 // CENTRAL API URL CONFIGURATION
 // Change this single variable when deploying your React frontend to Bluehost!
-export const API_BASE_URL = "http://localhost/E-commerce/backendDR/";
+export const API_BASE_URL = "https://hrntechsolutions.com/backendDR/";
+
